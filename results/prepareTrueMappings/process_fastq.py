7e981303cf8a9d122ce0d7baad36159d70ad522c
Tea Madzarac <tea.madzarac@fer.hr>
2025-01-24 17:53:37 +0100

    Update process_fastq.py

--- results/prepareTrueMappings/process_fastq.py
+++ results/prepareTrueMappings/process_fastq.py
@@ -1,25 +1,28 @@
 def process_file(input_file, output_file):
+    """
+    Initialize the estimator by parsing the SAM file
+    :param input_file: Path to the fastq file
+    :param output_file: Path to the .txt file with output of this function which shows correct mapping for each read
+    """
     with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
         while True:
             header = infile.readline().strip()  # Read the header line
             if not header:
-                break  # End of file
+                break
 
-            sequence = infile.readline().strip()  # Read the sequence line
+            sequence = infile.readline().strip()
 
-            # Split the header to extract the required parts
             header_parts = header.split()
             if len(header_parts) < 2:
-                continue  # Skip lines with unexpected format
+                continue
 
-            identifier = header_parts[0][1:]  # Remove the leading '@' from the identifier
+            identifier = header_parts[0][1:]
             taxid_info_parts = header_parts[1].split(",")
             if len(taxid_info_parts) < 1:
-                continue  # Skip lines with unexpected format
+                continue
 
-            taxid_info = taxid_info_parts[0]  # Extract the taxid info
+            taxid_info = taxid_info_parts[0]
 
-            # Write the processed line to the output file
             outfile.write(f"{identifier}\t{taxid_info}\n")
             
 input_file = "path/2bacteria4strains_c1.fastq"
